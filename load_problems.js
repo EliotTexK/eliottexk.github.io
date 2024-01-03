2aec84440383c40b5c5c60aa1df04312009e7d1b
Eliot <eaknxp@umsystem.edu>
2024-01-03 12:00:00 -0600

    add 10kindsofpeople.cpp

--- load_problems.js
+++ load_problems.js
@@ -1,53 +1,111 @@
+var daysLoaded = 0;
+
 async function loadProblems() {
-  const repo = "EliotTexK/eliottexk.github.io";
-  const dir = "problems/kattis";
-
-  // List files in /kattis
-  const res = await fetch(`https://api.github.com/repos/${repo}/contents/${dir}`);
-  const files = await res.json();
-
   // For each file, get last commit and file contents
-  const problems = await Promise.all(files.map(async (file) => {
-    const commitRes = await fetch(`https://api.github.com/repos/${repo}/commits?path=${dir}/${file.name}&per_page=1`);
-    const commits = await commitRes.json();
+  const kattisProblems = await getKattisProblems();
 
-    const contentRes = await fetch(file.download_url);
-    const content = await contentRes.text();
+  // TODO: other problem types go here
 
-    return {
-      name: file.name,
-      date: commits[0].commit.author.date,
-      content: content
-    };
-  }));
+  // Concatenate all problems
+  const problems = kattisProblems.concat([]);
 
   // Sort newest first
   problems.sort((a, b) => new Date(b.date) - new Date(a.date));
 
+  // Get all unique dates
+  const dates2Probs = mapDatesToProblems(problems)
+
   // Render
   const container = document.getElementById("problems");
   container.innerHTML = "";
-  
-  problems.forEach(p => {
-    const lang = getLanguage(p.name);
-    const div = document.createElement("div");
-    div.className = "solution";
-    
-    const h2 = document.createElement("h2");
-    h2.textContent = `${p.name} – ${new Date(p.date).toLocaleDateString()}`;
-    
-    const pre = document.createElement("pre");
-    const code = document.createElement("code");
-    code.className = `language-${lang}`;
-    code.textContent = p.content;
-    
-    pre.appendChild(code);
-    div.appendChild(h2);
-    div.appendChild(pre);
-    container.appendChild(div);
-  });
+
+  render5MoreDays(dates2Probs, container)
 
   Prism.highlightAll();
+}
+
+function render5MoreDays(dates2Probs, container) {
+  Array.from(dates2Probs.keys())
+  .sort((a, b) => b.localeCompare(a))
+  .slice(daysLoaded, daysLoaded+5)
+  .forEach(date => {
+    const todayProblems = dates2Probs.get(date);
+
+    const dateDiv = document.createElement("div");
+    const dateH1 = document.createElement("h1")
+    dateH1.textContent = `${date}`
+
+    dateDiv.appendChild(dateH1)
+    container.appendChild(dateDiv)
+
+    for (const problem of todayProblems) {
+      const renderedProblem = renderProblem(problem)
+      container.appendChild(renderedProblem)
+    }
+  });
+  daysLoaded += 5;
+}
+
+// Return a div with the problem rendered in it
+function renderProblem(problem) {
+  const div = document.createElement("div");
+  div.className = "solution";
+  
+  const h2 = document.createElement("h2");
+  h2.textContent = `${problem.name}`;
+  
+  const pre = document.createElement("pre");
+  const code = document.createElement("code");
+  code.className = `language-${problem.lang}`;
+  // code.textContent
+  
+  pre.appendChild(code);
+  div.appendChild(h2);
+  div.appendChild(pre);
+
+  return div
+}
+
+// Get a map from dates to problems
+function mapDatesToProblems(problems) {
+  const dateMap = new Map();
+  
+  for (const problem of problems) {
+    // Extract just the date part (YYYY-MM-DD) from the ISO timestamp
+    const date = problem.date.split('T')[0];
+    
+    if (!dateMap.has(date)) {
+      dateMap.set(date, []);
+    }
+    
+    dateMap.get(date).push(problem);
+  }
+  
+  return dateMap;
+}
+
+// Get a list of Kattis solutions in the repo
+async function getKattisProblems() {
+  const repo = "EliotTexK/eliottexk.github.io";
+  const dir = "problems/kattis";
+  const res = await fetch(`https://api.github.com/repos/${repo}/contents/${dir}`);
+  const files = await res.json();
+
+  const problems = await Promise.all(files.map(async (file) => {
+    const commitRes = await fetch(
+      `https://api.github.com/repos/${repo}/commits?path=${dir}/${file.name}&per_page=1`
+    );
+    const commits = await commitRes.json();
+    
+    return {
+      name: file.name,
+      date: commits[0].commit.author.date,
+      type: "Kattis",
+      lang: getLanguage(file.name),
+    };
+  }));
+  
+  return problems;
 }
 
 function getLanguage(filename) {
