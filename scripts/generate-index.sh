9c29b1d514a39adf188688cf2b4840e4a2bce58b
Eliot <eaknxp@umsystem.edu>
2025-10-04 14:06:16 -0500

    problem index to reduce API usage

---
+++ scripts/generate-index.sh
@@ -0,0 +1,20 @@
+mkdir -p problems
+echo "[" > problems/index.json
+first=true
+for file in $(find problems/kattis -type f); do
+    # Get last commit date in ISO format
+    date=$(git log -1 --format="%cI" -- "$file")
+    name=$(basename "$file")
+    lang="${name##*.}"
+
+    # Add comma except before first entry
+    if [ "$first" = true ]; then
+        first=false
+    else
+        echo "," >> problems/index.json
+    fi
+
+    # Escape double quotes in JSON strings
+    echo "  {\"type\": \"Kattis\", \"name\": \"$name\", \"date\": \"$date\", \"lang\": \"$lang\", \"solutionContentURL\": \"https://raw.githubusercontent.com/${{ github.repository }}/main/$file\"}" >> problems/index.json
+done
+echo "]" >> problems/index.json
